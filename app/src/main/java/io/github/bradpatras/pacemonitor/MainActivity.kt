61bf0e319dec326792e09d9fade8db2199a5a835
Brad <BradPatras@users.noreply.github.com>
2019-09-18 22:43:20 -0500

    initial project commit

---
+++ app/src/main/java/io/github/bradpatras/pacemonitor/MainActivity.kt
@@ -0,0 +1,19 @@
+package io.github.bradpatras.pacemonitor
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import io.github.bradpatras.pacemonitor.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+    }
+
+}
